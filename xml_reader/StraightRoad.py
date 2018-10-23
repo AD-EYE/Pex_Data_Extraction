b87aaf7563be6b7995b0fff5007314e4b54501ce
ornk <ornk@kth.se>
2018-10-23 15:04:40 +0200

    Created StraightRoad

---
+++ xml_reader/StraightRoad.py
@@ -0,0 +1,30 @@
+import math
+
+class StraightRoad(object):
+    def __init__(self, x, y, h_rad, length):
+        self.x = x
+        self.y = y
+        self.h_rad = h_rad
+        self.coords = []
+        self.length = length
+
+        self.create_road()
+
+    def get_coords(self):
+        return self.coords
+
+    def create_road(self):
+        number_of_points = math.ceil(self.length)
+        start = 0
+        incr = self.length/number_of_points
+
+        for _ in range(number_of_points):
+            self.coords.append(self.calculate_coord(start))
+            start = start + incr
+
+    def calculate_coord(self, length):
+        x1 = self.x + length * math.cos(self.h_rad)
+        y1 = self.x + length * math.sin(self.h_rad)
+
+        return [x1, y1]
+

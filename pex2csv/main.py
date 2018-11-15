5b490019071fb10637e0649289e218f07c6f55a4
ornk <ornk@kth.se>
2018-11-15 23:11:37 +0100

    Created a preprocessor for vector mapping, added multiple lanes functionality

--- pex2csv/main.py
+++ pex2csv/main.py
@@ -3,33 +3,16 @@ from vmap import VectorMap
 import numpy as np
 import parse
 import inspect
-
-def dist(p1, p2):
-    return np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
-
-def process_order(roads):
-    for id1 in roads.keys():
-        for id2 in roads.keys():
-            if id1 == id2: continue
-            if dist(roads[id1].getstart(), roads[id2].getstart()) <= 1.0:
-                roads[id1].previous_road = id2
-                roads[id2].previous_road = id1
-            elif dist(roads[id1].getstart(), roads[id2].getend()) <= 1.0:
-                roads[id1].previous_road = id2
-                roads[id2].next_road = id1
-            elif dist(roads[id1].getend(), roads[id2].getstart()) <= 1.0:
-                roads[id1].next_road = id2
-                roads[id2].previous_road = id1
-            elif dist(roads[id1].getend(), roads[id2].getend()) <= 1.0:
-                roads[id1].next_road = id2
-                roads[id2].next_road = id1
+from preproc import RoadProcessor
 
 if __name__ == '__main__':
     roads = parse.get_roads(path='./data/connected_roads1.pex')
+    rproc = RoadProcessor(roads)
 
     centers = []
     edges = []
     lanes = []
+    tlanes = rproc.get_lanes()
 
     for id in roads.keys():
         for (x, y) in roads[id].c:
@@ -44,16 +27,17 @@ if __name__ == '__main__':
                     edges.append([x, y])
             for (x, y) in roads[id].e2:
                 edges.append([x, y])
-            for (x, y) in roads[id].l1:
-                lanes.append([x, y])
-            for (x, y) in roads[id].l2:
-                lanes.append([x, y])
-            for path in roads[id].x:
-                for (x, y) in path:
-                    edges.append([x, y])
-            for path in roads[id].xl:
+            for path in roads[id].l:
                 for (x, y) in path:
                     lanes.append([x, y])
+            for road in roads[id].exit_lanes:
+                for (x, y) in road.e1:
+                    edges.append([x, y])
+                for (x, y) in road.e2:
+                    edges.append([x, y])
+                for path in road.l:
+                    for (x, y) in path:
+                        lanes.append([x, y])
         except Exception as e: print(e)
 
     centers = np.array(centers)
@@ -72,7 +56,8 @@ if __name__ == '__main__':
     ymin, ymax = plt.ylim()
 
     vm = VectorMap()
-    vm.make_lane(lanes, junction_end='RIGHT_MERGING')
+    for tlane in tlanes:
+        vm.make_lane(tlane, junction_end='RIGHT_MERGING')
     vm.make_line(edges, type='EDGE')
     vm.make_line(centers, type='CENTER')
     vm.export()
